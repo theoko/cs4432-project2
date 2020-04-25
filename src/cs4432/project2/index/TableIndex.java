a601eebb09c9f16c98d618fb104ec680db2b1670
Theodore Konstantopoulos <tkonstantopoulos@wpi.edu>
2020-04-24 20:10:32 -0400

    added README

--- src/cs4432/project2/index/TableIndex.java
+++ src/cs4432/project2/index/TableIndex.java
@@ -10,26 +10,6 @@ import java.util.List;
 import static cs4432.project2.utils.Helpers.extractRandomV;
 
 public class TableIndex {
-//    public List<DataFrame> getDataFrame(int search) {
-//        List<DataFrame> res = new ArrayList<>();
-//
-//        // Read all files in dataset directory
-//        int totalBlocks = Disk.INSTANCE.getTables().get(DiskFactory.DATASET_DIR).size();
-//        for (int j=1; j<=totalBlocks; j++) {
-//            String block4K = Disk.INSTANCE.readBlock(DiskFactory.DATASET_DIR, j);
-//            for (int i=1; i<=DiskFactory.RECORDS_PER_BLOCK; i++) {
-//                int start = 40 * (i - 1);
-//                int end = start + 40; // the first 40 bytes
-//                assert block4K != null;
-//                String recordContent = block4K.substring(start, end);
-//                // Extract randomV from record content
-//                int randomV = extractRandomV(recordContent);
-//                res.add(new DataFrame(j, i, randomV));
-//            }
-//        }
-//
-//        return res;
-//    }
 
     public List<DataFrame> searchForRandomV(int search) {
         List<DataFrame> res = new ArrayList<>();
